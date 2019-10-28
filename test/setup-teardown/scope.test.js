28c1ae1fd3c2d7643358574f2a254a38973136d5
cafefarm-johnny <npcdja@gmail.com>
2019-10-28 12:47:50 +0900

    훅 실행 순서 코드 분리

--- test/setup-teardown/scope.test.js
+++ test/setup-teardown/scope.test.js
@@ -99,21 +99,3 @@ function isValidCityFoodPair(city, food) {
 
     return false;
 }
-
-
-
-
-
-// 훅 실행 순서
-beforeAll(() => console.log('Global - beforeAll'));
-afterAll(() => console.log('Global - afterAll'));
-beforeEach(() => console.log('Global - beforeEach'));
-afterEach(() => console.log('Global - afterEach'));
-test('', () => console.log('Global - Test'));
-describe('Scoped / Nested block', () => {
-    beforeAll(() => console.log('describe - beforeAll'));
-    afterAll(() => console.log('describe - afterAll'));
-    beforeEach(() => console.log('describe - beforeEach'));
-    afterEach(() => console.log('describe - afterEach'));
-    test('', () => console.log('describe - Test'));
-});
